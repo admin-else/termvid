ab3170bb8fedc9b30e0e1d28089b08c4249c790a
Admin ELse <simon.kahre@gmail.com>
2024-02-08 00:14:49 +0100

    pls work

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'termvid=termvid:main'
+            'termvid=termvid.py:main'
         ],
     },
 )
